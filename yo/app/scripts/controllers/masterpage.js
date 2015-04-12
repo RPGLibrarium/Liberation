f0ec5c1ed35e0e9064469fa190aed157e32cd277
Yoann Kehler <privat@yoann.de>
2015-04-12 17:18:06 +0200

    Angular-Bootstrap manualy added, Navbar in Masterview The bower package "angular-bootstrap" is not compatible with our version of Angular. The bootstrap scripts written for Angular were added manualy. A first masterpage was implemented.

---
+++ yo/app/scripts/controllers/masterpage.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name liberationApp.controller:MasterpageCtrl
+ * @description
+ * # MasterpageCtrl
+ * Controller of the liberationApp
+ */
+angular.module('liberationApp')
+  .controller('MasterpageCtrl', function ($scope) {
+  	//Status of top navigation, if collapsed
+  	$scope.navbarMain = {isCollapsed: true};
+
+  	//Status of menuentry "Mein Konto"
+  	$scope.btnMeinKonto = {isopen: false};
+
+  	//Status of menuentry "Login"
+  	$scope.btnMeinKonto = {isopen: false};
+
+  	$scope.loginStatus = false;
+  });
